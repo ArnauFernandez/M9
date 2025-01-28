02a513cb1c3dd540b3d183d64da07a8d3446753e
arnau.fernandez.7e4 <arnau.fernandez.7e4@itb.cat>
2025-01-28 19:01:43 +0100

    M9 28/01/2025

---
+++ elmeusegonpython.py
@@ -0,0 +1,16 @@
+from flask import flask, render_template_string
+app=flask(__name__)
+@app.route('/')
+def home():
+    html_content= ""
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset ="UTF-8">
+    <title> Hola titol</title>
+    </head>
+    <body>
+    <h1>El meu segon python</h1>
+    <ul>
+
+    </body>
